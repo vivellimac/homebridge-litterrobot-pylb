3a00c214f979a65b22f5acd712893c714217de4c
vivellimac <114693343+vivellimac@users.noreply.github.com>
2025-09-28 10:25:30 -0400

    Create settings.js

---
+++ dist/settings.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PLATFORM_NAME = exports.PLUGIN_NAME = void 0;
+exports.PLUGIN_NAME = 'homebridge-litterrobot-pylb';
+exports.PLATFORM_NAME = 'LitterRobotPyLB';
