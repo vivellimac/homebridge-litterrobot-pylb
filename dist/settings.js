249aa2ff8a852dc68cbd005385ee09ccb13de8b1
vivellimac <114693343+vivellimac@users.noreply.github.com>
2025-09-28 15:12:12 -0400

    Update settings.js

--- dist/settings.js
+++ dist/settings.js
@@ -2,4 +2,4 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.PLATFORM_NAME = exports.PLUGIN_NAME = void 0;
 exports.PLUGIN_NAME = 'homebridge-litterrobot-pylb';
-exports.PLATFORM_NAME = 'LitterRobotPyLB';
+exports.PLATFORM_NAME = 'LitterRobotPylb';
