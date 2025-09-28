bdbded8a40bada3b886e31e105ade457da68edc1
vivellimac <114693343+vivellimac@users.noreply.github.com>
2025-09-28 10:25:03 -0400

    Create index.js

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const platform_1 = require("./platform");
+const settings_1 = require("./settings");
+module.exports = (api) => {
+  api.registerPlatform(settings_1.PLUGIN_NAME, settings_1.PLATFORM_NAME, platform_1.LitterRobotPlatform);
+};
