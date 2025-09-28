4d1a1d980f44fc52a12eae8f164926eb29397feb
vivellimac <114693343+vivellimac@users.noreply.github.com>
2025-09-28 10:20:54 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import type { API } from 'homebridge';
-import { LitterRobotPlatform } from './platform.js';
-import { PLATFORM_NAME } from './settings.js';
+import { PLATFORM_NAME, PLUGIN_NAME } from './settings';
+import { LitterRobotPlatform } from './platform';
 
-export default (api: API) => {
-  api.registerPlatform(PLATFORM_NAME, LitterRobotPlatform);
+export = (api: API) => {
+  api.registerPlatform(PLUGIN_NAME, PLATFORM_NAME, LitterRobotPlatform);
 };
